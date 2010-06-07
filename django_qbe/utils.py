7bc65f7cd2dfb630f5572524e76704e89a9ab665
Javier de la Rosa <versae@gmail.com>
2010-06-07 18:20:21 +0200

    Improving the auto-completion framework.

--- django_qbe/utils.py
+++ django_qbe/utils.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 import base64
 import pickle
+import random
 
+from collections import deque
+from copy import copy
 from itertools import combinations
 
 from django.db.models import get_models
@@ -23,90 +26,118 @@ try:
     formats = import_module(qbe_formats).formats
 except ImportError:
     from django_qbe.exports import formats
-# Makes pyflakes happy
-formats
+formats # Makes pyflakes happy
 
 
 def qbe_models(admin_site=None, only_admin_models=False, json=False):
-    app_models = get_models()
-    admin_models = admin_site and [m for m, a in admin_site._registry.items()]
+    app_models = get_models(include_auto_created=True, include_deferred=True)
+    app_models_with_no_includes = get_models(include_auto_created=False,
+                                             include_deferred=False)
+    if admin_site:
+        admin_models = [m for m, a in admin_site._registry.items()]
+    else:
+        admin_models = []
     if only_admin_models:
         app_models = admin_models
     graphs = {}
+
+    def get_field_attributes(field):
+        return {field.name: {
+            'name': field.name,
+            'type': type(field).__name__,
+            'blank': field.blank,
+            'label': u"%s" % field.verbose_name.capitalize(),
+        }}
+
+    def get_through_fields(field):
+        # Deprecated
+        through_fields = []
+        for through_field in field.rel.through._meta.fields:
+            label = through_field.verbose_name.capitalize()
+            through_fields_dic = {
+                'name': through_field.name,
+                'type': type(through_field).__name__,
+                'blank': through_field.blank,
+                'label': u"%s" % label,
+            }
+            if hasattr(through_field.rel, "to"):
+                through_rel = through_field.rel
+                through_mod = through_rel.to.__module__.split(".")[-2]
+                through_name = through_mod.capitalize()
+                through_target = {
+                    'name': through_name,
+                    'model': through_rel.to.__name__,
+                    'field': through_rel.get_related_field().name,
+                }
+                through_fields_dic.update({
+                    "target": through_target,
+                })
+            through_fields.append(through_fields_dic)
+        return through_fields
+
+    def get_target(field):
+        target = {
+            'name': field.rel.to.__module__.split(".")[-2].capitalize(),
+            'model': field.rel.to.__name__,
+            'field': field.rel.to._meta.pk.name,
+        }
+        if hasattr(field.rel, 'through'):
+            name = field.rel.through.__module__.split(".")[-2].capitalize()
+            target.update({
+                'through': {
+                    'name': name,
+                    'model': field.rel.through.__name__,
+                    'field': field.rel.through._meta.pk.name,
+                }
+            })
+        return target
+
+    def get_target_relation(field, extras=""):
+        target = get_target(field)
+        relation = {
+            'target': target,
+            'type': type(field).__name__,
+            'source': field.name,
+            'arrows': extras,
+        }
+        return target, relation
+
+    def add_relation(model, field, extras=""):
+        target, relation = get_target_relation(field, extras=extras)
+        if relation not in model['relations']:
+            model['relations'].append(relation)
+        model['fields'][field.name].update({'target': target})
+        return model
 
     for app_model in app_models:
         model = {
             'name': app_model.__name__,
             'fields': {},
             'relations': [],
-            'collapse': app_model not in admin_models,
+            'collapse': ((app_model not in admin_models) or
+                         (app_model not in app_models_with_no_includes)),
         }
 
-        # model attributes
-        def add_attributes():
-            model['fields'].update({field.name: {
-                'name': field.name,
-                'type': type(field).__name__,
-                'blank': field.blank,
-                'label': u"%s" % field.verbose_name.capitalize(),
-            }})
-
         for field in app_model._meta.fields:
-            add_attributes()
-
-        if app_model._meta.many_to_many:
-            for field in app_model._meta.many_to_many:
-                add_attributes()
-
-        # relations
-        def add_relation(extras=""):
-            target = {
-                'name': field.rel.to.__module__.split(".")[-2].capitalize(),
-                'model': field.rel.to.__name__,
-                'field': field.rel.get_related_field().name}
-            if hasattr(field.rel, 'through'):
-                # FIXME: Treatment of ManyToMany relations and through field
-                #        is not complete
-                through_fields = []
-                for through_field in field.rel.through._meta.fields:
-                    label = through_field.verbose_name.capitalize()
-                    through_fields.append({
-                        'name': through_field.name,
-                        'type': type(through_field).__name__,
-                        'blank': through_field.blank,
-                        'label': u"%s" % label,
-                    })
-                name = field.rel.through.__module__.split(".")[-2].capitalize()
-                target.update({
-                    'through': {
-                        'name': name,
-                        'model': field.rel.through.__name__,
-                        'fields': through_fields,
-                    }
-                })
-            _rel = {
-                'target': target,
-                'type': type(field).__name__,
-                'source': field.name,
-                'arrows': extras,
-            }
-            if _rel not in model['relations']:
-                model['relations'].append(_rel)
-            model['fields'][field.name].update({'target': target})
-
-        for field in app_model._meta.fields:
+            field_attributes = get_field_attributes(field)
+            model['fields'].update(field_attributes)
             if isinstance(field, ForeignKey):
-                add_relation()
+                model = add_relation(model, field)
             elif isinstance(field, OneToOneField):
-                add_relation("[arrowhead=none arrowtail=none]")
+                extras = "" # "[arrowhead=none arrowtail=none]"
+                model = add_relation(model, field, extras=extras)
 
         if app_model._meta.many_to_many:
             for field in app_model._meta.many_to_many:
+                field_attributes = get_field_attributes(field)
+                model['fields'].update(field_attributes)
                 if isinstance(field, ManyToManyField):
-                    add_relation("[arrowhead=normal arrowtail=normal]")
+                    extras = "" # "[arrowhead=normal arrowtail=normal]"
+                    model = add_relation(model, field, extras=extras)
                 elif isinstance(field, GenericRelation):
-                    add_relation(
-                        '[style="dotted"] [arrowhead=normal arrowtail=normal]')
+                    extras = "" # '[style="dotted"]
+                                # [arrowhead=normal arrowtail=normal]'
+                    model = add_relation(model, field, extras=extras)
 
         app_title = app_model._meta.app_label.title()
         if app_title not in graphs:
@@ -125,19 +156,107 @@ def qbe_graph(admin_site=None, directed=False):
     for k, v in models.items():
         for l, w in v.items():
             key = "%s.%s" % (k, l)
-            relations = w['relations']
             if key not in graph:
                 graph[key] = []
-            for r in relations:
-                value = "%s.%s" % (r['target']['name'], r['target']['model'])
+            for relation in w['relations']:
+                source = relation['source']
+                target = relation['target']
+                if "through" in target:
+                    through = target["through"]
+                    model = "%s.%s" % (through['name'], through['model'])
+                    value = (source, model, through['field'])
+                else:
+                    model = "%s.%s" % (target['name'], target['model'])
+                    value = (source, model, target['field'])
                 if value not in graph[key]:
                     graph[key].append(value)
                 if not directed:
-                    if value not in graph:
-                        graph[value] = []
-                    if key not in graph[value]:
-                        graph[value].append(key)
+                    if model not in graph:
+                        graph[model] = []
+                    target_field = target['field']
+                    target_value = (target_field, key, source)
+                    if target_value not in graph[model]:
+                        graph[model].append(target_value)
+            if not graph[key]:
+                del graph[key]
     return graph
+
+
+def qbe_tree(graph, nodes, root=None):
+    """
+    Given a graph, nodes to explore and an optinal root, do a breadth-first
+    search in order to return the tree.
+    """
+    if root:
+        start = root
+    else:
+        index = random.randint(0, len(nodes) - 1)
+        start = nodes[index]
+    # A queue to BFS instead DFS
+    to_visit = deque()
+    cnodes = copy(nodes)
+    visited = set()
+    # Format is (parent, parent_edge, neighbor, neighbor_field)
+    to_visit.append((None, None, start, None))
+    tree = {}
+    while len(to_visit) != 0 and nodes:
+        parent, parent_edge, v, v_edge = to_visit.pop()
+        # Prune
+        if v in nodes:
+            nodes.remove(v)
+        node = graph[v]
+        if v not in visited and len(node) > 1:
+            visited.add(v)
+            # Preorder process
+            if all((parent, parent_edge, v, v_edge)):
+                if parent not in tree:
+                    tree[parent] = []
+                if (parent_edge, v, v_edge) not in tree[parent]:
+                    tree[parent].append((parent_edge, v, v_edge))
+                if v not in tree:
+                    tree[v] = []
+                if (v_edge, parent, parent_edge) not in tree[v]:
+                    tree[v].append((v_edge, parent, parent_edge))
+            # Iteration
+            for node_edge, neighbor, neighbor_edge in node:
+                value = (v, node_edge, neighbor, neighbor_edge)
+                to_visit.append(value)
+    remove_leafs(tree, cnodes)
+    return tree, (len(nodes) == 0)
+
+
+def remove_leafs(tree, nodes):
+
+    def get_leafs(tree, nodes):
+        return [node for node, edges in tree.items()
+                     if len(edges) < 2 and node not in nodes]
+
+    def delete_edge_leafs(tree, leaf):
+        for node, edges in tree.items():
+            for node_edge, neighbor, neighbor_edge in edges:
+                if leaf == neighbor:
+                    edge = (node_edge, neighbor, neighbor_edge)
+                    tree[node].remove(edge)
+        del tree[leaf]
+
+    leafs = get_leafs(tree, nodes)
+    iterations = 0
+    while leafs or iterations > len(tree) ^ 2:
+        for node in leafs:
+            if node in tree:
+                delete_edge_leafs(tree, node)
+        leafs = get_leafs(tree, nodes)
+        iterations += 0
+    return tree
+
+
+def qbe_forest(graph, nodes):
+    forest = []
+    for node, edges in graph.items():
+        tree, are_all = qbe_tree(graph, copy(nodes), root=node)
+        if are_all and tree not in forest:
+            forest.append(tree)
+    return sorted(forest, cmp=lambda x, y: cmp(len(x), len(y)))
 
 
 def find_all_paths(graph, start, end, path=[]):
@@ -147,7 +266,7 @@ def find_all_paths(graph, start, end, path=[]):
     if start not in graph:
         return []
     paths = []
-    for node in graph[start]:
+    for source_field, (node, target_field) in graph[start].items():
         if node not in path:
             newpaths = find_all_paths(graph, node, end, path)
             for newpath in newpaths:
@@ -155,10 +274,10 @@ def find_all_paths(graph, start, end, path=[]):
     return paths
 
 
-def autocomplete_graph(admin_site, current_models):
+def autocomplete_graph(admin_site, current_models, directed=False):
     if len(current_models) < 2:
         return None
-    graph = qbe_graph(admin_site)
+    graph = qbe_graph(admin_site, directed=directed)
     valid_paths = []
     for c, d in combinations(current_models, 2):
         paths = find_all_paths(graph, c, d)
@@ -179,7 +298,7 @@ def pickle_encode(session_dict):
     return base64.encodestring(pickled + pickled_md5)
 
 
-# Taken from django.contrib.sessions.backends.base
+# Adapted from django.contrib.sessions.backends.base
 def pickle_decode(session_data):
     # The '+' character is translated to ' ' in request
     session_data = session_data.replace(" ", "+")
