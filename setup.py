980c2cd5086a091ee2e1878fdbf93446357d2f02
Jannis Leidel <jannis@leidel.info>
2012-06-18 11:42:08 +0200

    Moved version string to common practice location and bumped it to 0.2a2.

--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
-import os
-
+import codecs
+import re
+from os import path
 from setuptools import setup, find_packages
 
 
-def read(*rnames):
-    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+def read(*parts):
+    file_path = path.join(path.dirname(__file__), *parts)
+    return codecs.open(file_path).read()
+
+
+def find_version(*parts):
+    version_file = read(*parts)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
 
 setup(
     name='django_qbe',
-    version='0.2.0a1',
+    version=find_version('django_qbe', '__init__.py'),
     author='Javier de la Rosa',
     author_email='versae@gmail.com',
     url='http://versae.github.com/qbe/',
