1502c371d082d34daf8c703780dc272656b84786
Kyle Agronick <kagronick@cvs.com>
2017-07-11 11:02:16 -0400

    Rebase of #54 upon versae/master
    Fixed to work with newest version of Django.
    Using StringIO to write one line at a time before flushing it is very slow. Reports download at about 5kbps. Also, previously the reports were created in memory and large reports would cause the server to run out of memory. This change causes the results to be written to the response while they are being read from the database. It never runs out of memory and runs at 600kbps.
    The same change is on the HTML view as well. The same problem can present itself if the user selects to view all records. The raw cursor is passed instead of a list of results.

--- django_qbe/exports.py
+++ django_qbe/exports.py
@@ -1,22 +1,17 @@
-from future import standard_library
-standard_library.install_aliases()
-from builtins import str
-from builtins import object
 # -*- coding: utf-8 -*-
-import codecs
 import csv
-try:
-    # Use byte-oriented StringIO on Python 2!
-    from StringIO import StringIO
-except ImportError:
-    from io import StringIO
+import six
+from builtins import object, str
 
-from django.http import HttpResponse
-try:
-    from collections import OrderedDict
-except ImportError:
-    # Backward compatibility for Django prior to 1.7
-    from django.utils.datastructures import SortedDict as OrderedDict
+import collections
+import six
+from collections import OrderedDict as SortedDict
+from django.http import StreamingHttpResponse
+from future import standard_library
+from io import StringIO, BytesIO
+
+standard_library.install_aliases()
+
 
 __all__ = ("formats", )
 
@@ -25,13 +20,13 @@ class FormatsException(Exception):
     pass
 
 
-class Formats(OrderedDict):
+class Formats(SortedDict):
 
     def add(self, format):
         parent = self
 
         def decorator(func):
-            if callable(func):
+            if isinstance(func, collections.Callable):
                 parent.update({format: func})
             else:
                 raise FormatsException("func is not a function.")
@@ -43,30 +38,26 @@ formats = Formats()
 
 
 # Taken from http://docs.python.org/library/csv.html#csv-examples
-class UnicodeWriter(object):
+class UnicodeWriter:
     """
     A CSV writer which will write rows to CSV file "f",
     which is encoded in the given encoding.
     """
 
-    def __init__(self, f, dialect=csv.excel_tab, encoding="utf-8", **kwds):
+    def __init__(self, dialect=csv.excel_tab, encoding="utf-8", **kwds):
         # Redirect output to a queue
-        self.queue = StringIO()
+        self.queue = BytesIO() if six.PY2 else StringIO()
         self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
-        self.stream = f
-        self.encoder = codecs.getincrementalencoder(encoding)()
 
     def writerow(self, row):
-        self.writer.writerow([str(s).encode("utf-8") for s in row])
+        self.writer.writerow([str(s) for s in row])
+
+    def get_values(self):
         # Fetch UTF-8 output from the queue ...
-        data = self.queue.getvalue()
-        data = data.decode("utf-8")
-        # ... and reencode it into the target encoding
-        data = self.encoder.encode(data)
-        # write to the target stream
-        self.stream.write(data)
+        ret = self.queue.getvalue()
         # empty queue
         self.queue.truncate(0)
+        return ret.encode('utf-8').lstrip(b'\0')
 
     def writerows(self, rows):
         for row in rows:
@@ -74,31 +65,38 @@ class UnicodeWriter(object):
 
 
 def base_export(labels, results, dialect=csv.excel_tab):
-    output = StringIO()
-    w = UnicodeWriter(output, dialect=dialect)
+    w = UnicodeWriter(dialect=dialect)
+    count = 0
+    import ipdb; ipdb.set_trace()
     w.writerow(labels)
     for row in results:
+        count += 1
         w.writerow(row)
-    output.seek(0)
-    return output.read()
+
+        if count % 200 == 0:
+            yield w.get_values()
+
+    yield w.get_values()
+
+
+def make_attachment(response, ext):
+    response['Content-Disposition'] = 'attachment; filename=export.%s' % ext
+    return response
 
 
 @formats.add("csv")
 def csv_format(labels, results):
-    output = base_export(labels, results, dialect=csv.excel)
     content_type = "text/csv"
-    return HttpResponse(output, content_type=content_type)
+    return make_attachment(StreamingHttpResponse(base_export(labels, results, dialect=csv.excel), content_type=content_type), "csv")
 
 
 @formats.add("ods")
 def ods_format(labels, results):
-    output = base_export(labels, results)
     content_type = "application/vnd.oasis.opendocument.spreadsheet"
-    return HttpResponse(output, content_type=content_type)
+    return make_attachment(StreamingHttpResponse(base_export(labels, results, dialect=csv.excel), content_type=content_type), "ods")
 
 
 @formats.add("xls")
 def xls_format(labels, results):
-    output = base_export(labels, results)
     content_type = "application/vnd.ms-excel"
-    return HttpResponse(output, content_type=content_type)
+    return make_attachment(StreamingHttpResponse(base_export(labels, results, dialect=csv.excel), content_type=content_type), "xls")
